cca180bd61d30278af359a87b7014a74c4e874d0
Hugo Xiong <hugoxiong@gmail.com>
2020-09-30 06:14:28 -0700

    add product and style id matching to unit test

--- server/index.test.js
+++ server/index.test.js
@@ -8,10 +8,12 @@ afterAll(() => {
 
 describe("Photo API", () => {
   test("should get correct photo for a product + style id", async () => {
-    // const result = await request(app).get("/photos/XX1111/001");
-    // console.log(result);
-    // expect(result.product_id).toEqual("XX1111");
-    await request(app).get("/photos/XX1111/001").expect(200);
+    const response = await request(app).get("/photos/XX1111/001").expect(200);
+    const data = response.body;
+    expect(data).toMatchObject({
+      product_id: "XX1111",
+      style_id: "001",
+    })
   })
 
   test("should get 400 error for non-existent id", async () => {

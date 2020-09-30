7b247c02c8f6cabf3ccc174f2a169caf65d8fa32
Hugo Xiong <hugoxiong@gmail.com>
2020-09-30 06:15:58 -0700

    refactor out id mock data

--- server/index.test.js
+++ server/index.test.js
@@ -2,13 +2,16 @@ const request = require("supertest");
 const app = require("./app.js");
 const db = require("../database/connect.js");
 
+const product_id = "XX1111";
+const style_id = "001";
+
 afterAll(() => {
   db.close();
 });
 
 describe("Photo API", () => {
   test("should get correct photo for a product + style id", async () => {
-    const response = await request(app).get("/photos/XX1111/001").expect(200);
+    const response = await request(app).get(`/photos/${product_id}/${style_id}`).expect(200);
     const data = response.body;
     expect(data).toMatchObject({
       product_id: "XX1111",
@@ -17,6 +20,6 @@ describe("Photo API", () => {
   })
 
   test("should get 400 error for non-existent id", async () => {
-    await request(app).get("/photos/XX1111/doesntexist").expect(400);
+    await request(app).get(`/photos/${product_id}/doesntexist`).expect(400);
   })
 })

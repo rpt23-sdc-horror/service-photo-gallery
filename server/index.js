eba37b4377b17740a81cadd7ab892db14acf4604
Hugo Xiong <hugoxiong@gmail.com>
2020-09-29 17:55:29 -0700

    add database folder and file

--- server/index.js
+++ server/index.js
@@ -4,26 +4,30 @@
 // Response object (JSON):
 const response = {
   _id: `<Mongo ObjectID>`,
-  product_style_id: "CJ6741-003",
+  product_id: "CJ6741",
+  style_id: "003",
   main_photo: {
+    // for the MAIN photo url, the AWS S3 path is based on productid, styleid, and size. This always follows a predictable pattern:
+    // URL: /photos/CJ6741-003/main/thumbnail
+    // URL: /photos/CJ6741-003/main/regular
     _id: `<Mongo ObjectID>`,
-    thumbnail_url: "https://ultimate_nike.s3.amazonaws.com/photos/FlI7pOPrf",
-    regular_url: "https://ultimate_nike.s3.amazonaws.com/photos/ak7LMO43d",
+    thumbnail_url: "https://ultimate_nike.s3.amazonaws.com/photos/main_thumbnail/CJ6741-003.jpg",
+    regular_url: "https://ultimate_nike.s3.amazonaws.com/photos/main_regular/CJ6741-003.jpg",
   },
   other_photos: [
     // these are extra photos different from the main photo
     // only main photo will have thumbnail size
     {
       _id: `<Mongo ObjectID>`,
-      regular_url: "https://ultimate_nike.s3.amazonaws.com/photos/ak7LMO43d",
+      regular_url: "https://ultimate_nike.s3.amazonaws.com/photos/CJ6741-003/other/ak7LMO43d",
     },
     {
       _id: `<Mongo ObjectID>`,
-      regular_url: "https://ultimate_nike.s3.amazonaws.com/photos/NHV4Lksp2",
+      regular_url: "https://ultimate_nike.s3.amazonaws.com/photos/CJ6741-003/other/NHV4Lksp2",
     },
     {
       _id: `<Mongo ObjectID>`,
-      regular_url: "https://ultimate_nike.s3.amazonaws.com/photos/NB21suRE4",
+      regular_url: "https://ultimate_nike.s3.amazonaws.com/photos/CJ6741-003/other/NB21suRE4",
     },
   ],
 };
@@ -57,24 +61,24 @@ const response = {
   list: [
     {
       _id: `<Mongo ObjectID>`,
-      photo_url: "https://ultimate_nike.s3.amazonaws.com/photos/ak7LMO43d",
+      photo_url: "https://ultimate_nike.s3.amazonaws.com/photos/instagram/ak7LMO43d",
       date_posted:
         "<Date object> Thu Sep 24 2020 15:53:54 GMT-0700 (Pacific Daylight Time)",
       likes: 15,
       user: {
         name: "windzchill",
-        avatar_url: "https://ultimate_nike.s3.amazonaws.com/photos/007",
+        avatar_url: "https://ultimate_nike.s3.amazonaws.com/photos/instagram/avatar/007",
       },
     },
     {
       _id: `<Mongo ObjectID>`,
-      photo_url: "https://ultimate_nike.s3.amazonaws.com/photos/NHV4Lksp2",
+      photo_url: "https://ultimate_nike.s3.amazonaws.com/photos/instagram/NHV4Lksp2",
       date_posted:
         "<Date object> Wed Oct 02 2017 13:42:98 GMT-0700 (Pacific Daylight Time)",
       likes: 223,
       user: {
         name: "michael.dieseltraining",
-        avatar_url: "https://ultimate_nike.s3.amazonaws.com/photos/008",
+        avatar_url: "https://ultimate_nike.s3.amazonaws.com/photos/instagram/avatar/008",
       },
     },
   ],

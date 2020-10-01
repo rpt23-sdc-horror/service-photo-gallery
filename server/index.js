39c1227509a76fd9a909fda2b4d3b99be9b10074
Hugo Xiong <hugoxiong@gmail.com>
2020-09-30 22:13:30 -0700

    add eslint plugin for jest

--- server/index.js
+++ server/index.js
@@ -1,11 +1,11 @@
 // express + routes
-const app = require("./app");
+const app = require('./app');
 
 // open connection to Photos DB
-require("../database/connect.js");
+require('../database/connect.js');
 
 const port = 3000;
 
 app.listen(port, () => {
-  console.log(`Photo service listening on ${port}`)
-})
+  console.log(`Photo service listening on ${port}`);
+});

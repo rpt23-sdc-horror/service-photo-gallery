9ea3319bb6831415a2e03e6f259f5a1fcb90f59a
Hugo Xiong <hugoxiong@gmail.com>
2020-10-06 20:45:30 -0700

    add enyzme test for photo card loading

--- client/App.test.js
+++ client/App.test.js
@@ -2,6 +2,18 @@ import App from "./App.jsx";
 
 const app = mount(<App />);
 
+const photos = [
+  "https://ultimate-nike.s3.us-west-1.amazonaws.com/photos/main/regular/1-001.jpg",
+  "https://ultimate-nike.s3.us-west-1.amazonaws.com/photos/main/regular/98-002.jpg",
+  "https://ultimate-nike.s3.us-west-1.amazonaws.com/photos/main/regular/31-001.jpg",
+  "https://ultimate-nike.s3.us-west-1.amazonaws.com/photos/main/regular/1-003.jpg",
+  "https://ultimate-nike.s3.us-west-1.amazonaws.com/photos/main/regular/72-003.jpg",
+  "https://ultimate-nike.s3.us-west-1.amazonaws.com/photos/main/regular/30-003.jpg",
+  "https://ultimate-nike.s3.us-west-1.amazonaws.com/photos/main/regular/66-002.jpg",
+  "https://ultimate-nike.s3.us-west-1.amazonaws.com/photos/main/regular/85-001.jpg",
+  "https://ultimate-nike.s3.us-west-1.amazonaws.com/photos/main/regular/57-002.jpg"
+];
+
 beforeEach(() => {
   // clear component state
   app.unmount();
@@ -24,3 +36,24 @@ describe("Component rendering", () => {
     expect(app.find("#photo-modal")).toHaveClassName("hidden");
   });
 });
+
+describe("Photo Gallery", () => {
+  beforeEach(() => {
+    app.setState({
+      photos
+    });
+  });
+
+  it('should render photos in gallery view', () => {
+    const gallery = app.find("#gallery");
+    expect(gallery.children()).toHaveLength(9);
+  })
+
+  it('should render a photo card with img url', () => {
+    const gallery = app.find("#gallery");
+    const card = gallery.find(".photo-card").first();
+    expect(card).toHaveHTML(
+      '<div class="photo-card"><img src="https://ultimate-nike.s3.us-west-1.amazonaws.com/photos/main/regular/1-001.jpg" data-index="0" alt="this is a description"></div>'
+    );
+  })
+})

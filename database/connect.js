ed1f7e6943ebb1f8fa5eab25857e3009560fa6a2
Tandy Nguyen <nguyetan001@gmail.com>
2020-11-17 02:57:55 -0500

    Change api endpoint & db to localhost

--- database/connect.js
+++ database/connect.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 // connection string for DOCKER...
-mongoose.connect('mongodb://mongo:27017', {
+mongoose.connect('mongodb://localhost:27017/photos', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useFindAndModify: false,
